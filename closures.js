cb7a978015a317da0218e5271e4f43b43859f2d6
Austin <brake_austin@yahoo.com>
2018-11-08 16:29:26 -0700

    finished afternoon

--- closures.js
+++ closures.js
@@ -23,13 +23,12 @@ function outer() {
 */
   
 // Code Here
-
-
+var inner = outer()
 
 //Once you do that, invoke inner.
 
 //Code Here
-
+inner()
 
 
 ////////// PROBLEM 2 //////////
@@ -53,7 +52,7 @@ function callFriend(name) {
 
 //Code Here
 
-
+var callJake = callFriend("Jake")
 
 ////////// PROBLEM 3 //////////
 
@@ -62,15 +61,21 @@ function callFriend(name) {
 */
 
 //Code Here
+function makeCounter(){
+  var num = 0
+  return function(){
+    num ++
+    return num
+  }
+}
 
 
-
-//Uncomment this once you make your function
-//   var count = makeCounter();
-//   count(); // 1
-//   count(); // 2
-//   count(); // 3
-//   count(); // 4
+// Uncomment this once you make your function
+  // var count = makeCounter();
+  // count(); // 1
+  // count(); // 2
+  // count(); // 3
+  // count(); // 4
 
 
 
@@ -86,18 +91,23 @@ function callFriend(name) {
 */
 
 function counterFactory(value) {
-  // Code here.
+  
 
   return {
-
+      inc(){
+        return ++value
+      },
+      dec(){
+        return --value
+      }
   };
 }
 
 counter = counterFactory(10);
-// counter.inc() // 11
-// counter.inc() // 12
-// counter.inc() // 13
-// counter.dec() // 12
+counter.inc() // 11
+counter.inc() // 12
+counter.inc() // 13
+counter.dec() // 12
 
 
 
@@ -112,10 +122,10 @@ counter = counterFactory(10);
 function motivation( firstname, lastname ) {
   var welcomeText = "You're doing awesome, keep it up";
 
-  // code message function here.
-
-  //Uncommment this to return the value of your message function
-  //return message;
+    function message(){
+      return welcomeText + ' ' + firstname + ' ' + lastname + '.'
+    }
+  return message;
 }
 
 var greeting = motivation('Billy', 'Bob'); // 'You're doing awesome keep it up Billy Bob.
@@ -143,7 +153,9 @@ var module = (function() {
   // Anything that is being returned is made public and can be invoked from
   // outside our lexical scope
   return {
-    // Code here.
+    publicMethod(){
+      return privateMethod()
+    }
   };
 })();
 
@@ -162,7 +174,14 @@ function secretNumber() {
   var secret = 143;
 
   return {
-    // Code here
+    addToSecret(num){
+      return secret += num
+      
+    },
+    takeAwayFromSecret(num){
+      return secret -= num
+      
+    }
   };
 }
 
@@ -182,15 +201,18 @@ function secretNumber() {
 
   However, because each call to console.log occurs after the loop has finished, the value of i has changed before the console.log executes.
   We'll need to use a closure to preserve a reference to i at the time of execution.
-  
+
   Fix the code below to log the desired output.
 */
 
 function timeOutCounter() {
   for (var i = 0; i <= 5; i++) {
-    setTimeout(function() {
-      console.log(i);
-    }, i * 1000);
+    function closure(i){
+      setTimeout(function() {
+       console.log(i);
+      }, i * 1000);
+    }
+    closure(i)
   }
 }
 timeOutCounter();
--- constructors.js
+++ constructors.js
@@ -12,7 +12,10 @@
   Create a constructor function called CarFactory that will build car objects with make and model properties.
   The values for the make and model will come from two parameters, make and model (in that order).
 */
-
+function CarFactory( make, model ){
+  this.make = make,
+  this.model = model
+}
 // Code here
 
 
@@ -31,7 +34,7 @@ function Employee(name, email, hireDate) {
   Invoke the constructor function above and pass in 'Bob', 'bob@gmail.com', and '01-02-98' as arguments.
   Assign the result of the invocation to a variable called bob.
 */
-
+var bob = new Employee('Bob', 'bob@gmail.com', '01-02-98')
 // Code here
 
 
@@ -55,4 +58,17 @@ mustang.moveCar(); // Increments mustang' move property by 10. Returns the new m
 
 // Code here
 
+function Car(make, model, year){
+  this.make = make,
+  this.model = model,
+  this.year = year,
+  this.move = 0
+  
+  this.moveCar = function(){
+    return this.move += 10
+    
+
+  }
+  // moveCar()
+}
 
